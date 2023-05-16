137a92b14b851a4ea1e7c9f7641a090c584e811e
Dmitrii Okunev <xaionaro@meta.com>
2023-05-16 22:30:40 +0100

    updated

--- pkg/observability/logger.go
+++ pkg/observability/logger.go
@@ -4,20 +4,20 @@ import (
 	"context"
 
 	"github.com/facebookincubator/go-belt/tool/logger"
-	"github.com/facebookincubator/go-belt/tool/logger/adapter"
 	"github.com/facebookincubator/go-belt/tool/logger/implementation/logrus"
 	"github.com/facebookincubator/go-belt/tool/logger/types"
 
 	"github.com/immune-gmbh/AttestationFailureAnalysisService/pkg/observability/hooks/logentryfingerprint"
+	"github.com/immune-gmbh/AttestationFailureAnalysisService/pkg/observability/tool/logger/logrus/formatter"
 )
 
 // NewLogger returns the default Logger for family of applications
 // based on AttestationFailureAnalysisService.
 func NewLogger(ctx context.Context, opts ...types.Option) logger.Logger {
-	logrusEmitter := logrus.NewEmitter(logrus.DefaultLogrusLogger())
+	l := logrus.DefaultLogrusLogger()
+	l.Formatter = &formatter.CompactText{}
 
-	var result logger.Logger
-	result = adapter.LoggerFromEmitter(logrusEmitter)
+	result := logrus.New(l)
 	result = result.WithPreHooks(logentryfingerprint.PreHook{})
 	result = result.WithLevel(logger.LevelTrace)
 	return result
